c9060e94f151124d440d4b6c71eef1511640e0fc
Maira <maira.tolentino@hotmail.com>
2020-10-31 11:18:55 -0300

    creating Level and ItemLearned classes

---
+++ WhatDidILearn/app/src/main/java/com/wcc/whatdidilearn/entities/ItemLearned.kt
@@ -0,0 +1,7 @@
+package com.wcc.whatdidilearn.entities
+
+data class ItemLearned(
+    val title: String,
+    val description: String,
+    val understandingLevel: Level
+)
---
+++ WhatDidILearn/app/src/main/java/com/wcc/whatdidilearn/entities/Level.kt
@@ -0,0 +1,7 @@
+package com.wcc.whatdidilearn.entities
+
+enum class Level {
+    HIGH,
+    MEDIUM,
+    LOW
+}
