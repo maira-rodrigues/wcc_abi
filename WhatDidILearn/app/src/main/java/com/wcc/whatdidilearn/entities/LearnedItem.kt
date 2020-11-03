5df279772066db10b38d22ab247ebfc1f31be24a
Maira <maira.tolentino@hotmail.com>
2020-11-03 20:57:10 -0300

    Creating room entity

--- WhatDidILearn/app/src/main/java/com/wcc/whatdidilearn/entities/LearnedItem.kt
+++ WhatDidILearn/app/src/main/java/com/wcc/whatdidilearn/entities/LearnedItem.kt
@@ -1,7 +1,21 @@
 package com.wcc.whatdidilearn.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "learned_item")
 data class LearnedItem(
+    @ColumnInfo(name = "item_title")
     val title: String,
+
+    @ColumnInfo(name = "item_description")
     val description: String,
-    val understandingLevel: Level
+
+    @ColumnInfo(name ="item_understanding_level")
+    val understandingLevel: Level,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "item_id")
+    val id: Int = 0
 )
