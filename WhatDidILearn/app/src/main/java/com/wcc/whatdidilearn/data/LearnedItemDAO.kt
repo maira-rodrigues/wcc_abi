09ab13f33f294856628a4f6a7f45c3d0c2b81ad7
Maira <maira.tolentino@hotmail.com>
2020-11-03 21:44:05 -0300

    creating LearnedItem DAO

---
+++ WhatDidILearn/app/src/main/java/com/wcc/whatdidilearn/data/LearnedItemDAO.kt
@@ -0,0 +1,15 @@
+package com.wcc.whatdidilearn.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.wcc.whatdidilearn.entities.LearnedItem
+
+@Dao
+interface LearnedItemDAO {
+    @Query("SELECT * FROM learned_item ORDER BY item_title ASC")
+    fun getAll(): List<LearnedItem> //não precisa implementar função pq é Interface
+
+    @Insert()
+    fun insert(item: LearnedItem)
+}
